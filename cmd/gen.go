0c9cfcba7b2ceb37a799d6bc667bee3cdba8ec82
Pierre Ugaz <me@ulm0.com>
2024-09-30 20:09:47 -0300

    chore: add commands

--- cmd/gen.go
+++ cmd/gen.go
@@ -1,5 +1,4 @@
-The MIT License (MIT)
-
+/*
 Copyright © 2024 ulm0
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,36 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// genCmd represents the gen command
+var genCmd = &cobra.Command{
+	Use:   "gen",
+	Short: "generate docs",
+	Long: `generate docs for your go project
+either local or remote.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gen called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(genCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
