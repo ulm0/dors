84df677de7e5d24e6cf02a6bab9fa0cde6f9ddb2
Pierre Ugaz <me@ulm0.com>
2024-09-30 21:35:27 -0300

    chore: remove utils.go from opentelemetry

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/ulm0/dors
 
 go 1.23.1
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.8.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
---
+++ pkg/gen/run.go
@@ -0,0 +1,46 @@
+package gen
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/gddo/doc"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func get(ctx context.Context, client *http.Client, name, tag string) (*doc.Package, error) {
+	p, err := doc.Get(ctx, client, name, tag)
+	if err != nil {
+		return nil, err
+	}
+	if err = workaroundLocalSubDirs(p, name); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func workaroundLocalSubDirs(p *doc.Package, pkg string) error {
+	if !strings.HasPrefix(pkg, ".") {
+		return nil // Not local
+	}
+
+	files, err := os.ReadDir(p.ImportPath)
+	if err != nil {
+		return fmt.Errorf("failed reading import path %s: %w", p.ImportPath, err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			p.Subdirectories = append(p.Subdirectories, f.Name())
+		}
+	}
+	return nil
+}
+
+func Called() func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		fmt.Println("gen called")
+	}
+}
